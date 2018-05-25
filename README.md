fe3091cb332d6c676415d8f8013f2086a22fe9ab
fmassot <fmassot@meilleurecopro.com>
2018-05-25 17:11:22 +0200

    Update example

--- README.md
+++
@@ -1,5 +0,0 @@
-## Example d'autocomplete avec google maps et redirection avec le formulaire d'estimation
-
-1. Ouvrir le fichier index.html dans un navigateur
-2. Rentrez une adresse
-3. Sélectionner une réponse et attendez quelques secondes avant la redirection vers le formulaire
--- gmaps-autocomplete.js
+++ gmaps-autocomplete.js
@@ -2,37 +2,54 @@ function autocompleteWidget(input) {
     var autocomplete = new google.maps.places.Autocomplete(
         input,
         {componentRestrictions: {country: "fr"}, types: ['address']}),
-        baseUrl = 'https://preprod.meilleurecopro.com/estimation-charges-copropriete-widget';
+        baseUrl = 'https://www.meilleurecopro.com/estimation-charges-copropriete-widget';
 
-    function buildFullUrl(place) {
-        var queryParams = '?address=' + encodeURIComponent(place.formatted_address);
+    function getAddress(place) {
+        var address = {
+            fullAddress: place.formatted_address
+        };
+
+        var queryParams = 'address=' + encodeURIComponent(place.formatted_address);
         queryParams += '';
 
         for (var i = 0; i < place.address_components.length; i++) {
             var component = place.address_components[i];
 
-            if (component.types.indexOf('street_number') !== -1)
+            if (component.types.indexOf('street_number') !== -1) {
                 queryParams += '&addressNumber=' + encodeURIComponent(component['long_name']);
+                address.addressNumber = component['long_name'];
+            }
 
-            if (component.types.indexOf('route') !== -1)
+            if (component.types.indexOf('route') !== -1) {
                 queryParams += '&addressStreet=' + encodeURIComponent(component['long_name']);
+                address.addressStreet = component['long_name'];
+            }
 
-            if (component.types.indexOf('locality') !== -1)
+            if (component.types.indexOf('locality') !== -1) {
                 queryParams += '&addressCity=' + encodeURIComponent(component['long_name']);
+                address.addressCity = component['long_name'];
+            }
 
             if (component.types.indexOf('postal_code') !== -1) {
                 queryParams += '&addressZipCode=' + encodeURIComponent(component['long_name']);
-                queryParams += '&addressDepartment=' + encodeURIComponent(component['long_name']).slice(0, 2);
+                queryParams += '&addressDepartment=' + encodeURIComponent(component['long_name'].slice(0, 2));
+                address.addressZipCode = component['long_name'];
+                address.addressDepartment = component['long_name'].slice(0, 2);
             }
 
-            if (component.types.indexOf('country') !== -1)
+            if (component.types.indexOf('country') !== -1) {
                 queryParams += '&addressCountry=' + encodeURIComponent(component['long_name']);
+                address.addressCountry = component['long_name'];
+            }
         }
 
         queryParams += '&lat=' + place.geometry.location.lat();
         queryParams += '&lng=' + place.geometry.location.lng();
+        address.lat = place.geometry.location.lat();
+        address.lng = place.geometry.location.lng();
+        address.queryParams = queryParams;
 
-        return baseUrl + queryParams;
+        return address;
     }
 
     google.maps.event.addListener(autocomplete, 'place_changed', function () {
@@ -46,13 +63,20 @@ function autocompleteWidget(input) {
 
         input.value = place.formatted_address;
 
-        var url = buildFullUrl(place);
+        var address = getAddress(place);
 
-        console.log('redirect to url in 3 seconds: ', url);
-
-        setTimeout(function() { document.location = url; }, 3000);
+        document.getElementById('iframeUrl').value = baseUrl + '?' + address.queryParams;
+        document.getElementById('simulation-widget').src = baseUrl + '?' + address.queryParams;
+        document.getElementById('address').value = address.fullAddress;
+        document.getElementById('addressNumber').value = address.addressNumber;
+        document.getElementById('addressStreet').value = address.addressStreet;
+        document.getElementById('addressCity').value = address.addressCity;
+        document.getElementById('addressDepartment').value = address.addressDepartment;
+        document.getElementById('addressZipCode').value = address.addressZipCode;
+        document.getElementById('addressCountry').value = address.addressCountry;
+        document.getElementById('lat').value = address.lat;
+        document.getElementById('lng').value = address.lng;
     });
 }
 
-console.log(document.getElementById('address-input'));
 autocompleteWidget(document.getElementById('address-input'));
